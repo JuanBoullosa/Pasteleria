91a768c10183a7638a0c7d2a249d02fe1f4b9bd6
Fede1891 <160802959+Fede1891@users.noreply.github.com>
2026-02-16 20:23:14 -0300

    Update VOPostreDetallado.java

--- Pasteleria/src/logica/valueobjects/VOPostreDetallado.java
+++ Pasteleria/src/logica/valueobjects/VOPostreDetallado.java
@@ -1,5 +1,46 @@
 package logica.valueobjects;
 
 public class VOPostreDetallado {
+	
+	private float precio;
+	private String nombre;
+	private String codigo;
+	private String tipo;
+	
+	
+	public VOPostreDetallado(float precio, String nombre, String codigo, String tipo) {
+		super();
+		this.precio = precio;
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.tipo = tipo;
+	}
+	
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 
 }
