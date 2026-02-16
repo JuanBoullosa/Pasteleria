66e5447dee7ca28dae0e4c8f27fa165204c1cb78
Fede1891 <160802959+Fede1891@users.noreply.github.com>
2026-02-16 20:08:53 -0300

    vo

--- Pasteleria/src/logica/valueobjects/VOVentaIngreso.java
+++ Pasteleria/src/logica/valueobjects/VOVentaIngreso.java
@@ -1,5 +1,31 @@
 package logica.valueobjects;
 
-public class VOVentaIngreso {
+import java.time.LocalDateTime;
 
+public class VOVentaIngreso {
+	
+	
+	
+	public VOVentaIngreso(String direccionEnvio, LocalDateTime fecha) {
+		super();
+		this.direccionEnvio = direccionEnvio;
+		this.fecha = fecha;
+	}
+	private String direccionEnvio;
+	private LocalDateTime fecha;
+	
+	public String getDireccionEnvio() {
+		return direccionEnvio;
+	}
+	public void setDireccionEnvio(String direccionEnvio) {
+		this.direccionEnvio = direccionEnvio;
+	}
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	
 }
