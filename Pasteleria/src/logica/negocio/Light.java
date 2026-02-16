ed23af523520a0e2c677f601bb90b407ca5587db
Fede1891 <160802959+Fede1891@users.noreply.github.com>
2026-02-16 20:53:07 -0300

    clases

--- Pasteleria/src/logica/negocio/Light.java
+++ Pasteleria/src/logica/negocio/Light.java
@@ -1,5 +1,7 @@
 package logica.negocio;
 
+
 public class Light {
+	
 
 }
--- Pasteleria/src/logica/negocio/Venta.java
+++ Pasteleria/src/logica/negocio/Venta.java
@@ -1,5 +1,58 @@
 package logica.negocio;
 
+import java.time.LocalDateTime;
+
 public class Venta {
+	
+	private int numero;
+	private LocalDateTime fecha;
+	private String direccion;
+	private String Estado;
+	private float montoTotal;
+	public int getNumero() {
+		return numero;
+	
+	}
+	
+	
+	public Venta(int numero, LocalDateTime fecha, String direccion, String estado, float montoTotal) {
+		super();
+		this.numero = numero;
+		this.fecha = fecha;
+		this.direccion = direccion;
+		Estado = estado;
+		this.montoTotal = montoTotal;
+	}
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getEstado() {
+		return Estado;
+	}
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	public float getMontoTotal() {
+		return montoTotal;
+	}
+	public void setMontoTotal(float montoTotal) {
+		this.montoTotal = montoTotal;
+	}
+	
+	
 
 }
