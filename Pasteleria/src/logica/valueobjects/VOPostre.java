e132bb6a762f638903c3067dd251798bc3ca9da3
JuanBoullosa <151090164+JuanBoullosa@users.noreply.github.com>
2026-02-16 20:09:58 -0300

    Update VOPostre.java

--- Pasteleria/src/logica/valueobjects/VOPostre.java
+++ Pasteleria/src/logica/valueobjects/VOPostre.java
@@ -2,4 +2,39 @@ package logica.valueobjects;
 
 public class VOPostre {
 
+	private float precio;
+	private String nombre;
+	private	String codigo;
+	
+	
+	
+	public VOPostre(float precio, String nombre, String codigo) {
+		super();
+		this.precio = precio;
+		this.nombre = nombre;
+		this.codigo = codigo;
+	}
+	
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+	
+	
 }
