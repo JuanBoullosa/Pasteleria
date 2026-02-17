b541d97ae06a1ebf9ab5df0cfbefd48148a213b2
JuanBoullosa <151090164+JuanBoullosa@users.noreply.github.com>
2026-02-17 10:02:02 -0300

    Update Postres.java

--- Pasteleria/src/logica/colecciones/Postres.java
+++ Pasteleria/src/logica/colecciones/Postres.java
@@ -1,6 +1,35 @@
 package logica.colecciones;
+import java.util.TreeMap;
+import logica.negocio.Postre;
 
 public class Postres {
 	
+	private TreeMap<String,Postre> abb;
+	
 
+	public Postres ()
+	{
+		abb = new TreeMap<String, Postre> ();
+	}
+
+	public boolean member(String codigo)
+	{
+		return abb.containsKey(codigo);
+	}
+	
+	public Postre find (String codigo)
+	{
+		return abb.get(codigo);
+	}
+	
+	public void insert (String codigo, Postre P)
+	{
+		abb.put(codigo, P);
+	}
+	
+	public void detelet (String codigo)
+	{
+		abb.remove(codigo);
+	}
+	
 }
