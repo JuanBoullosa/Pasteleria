e7f52c75bdcf941b216cd1e8f2272c4d22e8e3e3
Fede1891 <160802959+Fede1891@users.noreply.github.com>
2026-02-16 21:08:00 -0300

    Create Es_Vendido.java

---
+++ Pasteleria/src/logica/negocio/Es_Vendido.java
@@ -0,0 +1,24 @@
+package logica.negocio;
+
+public class Es_Vendido {
+	
+	private int cantidad;
+	
+	
+
+	public Es_Vendido(int cantidad) {
+		super();
+		this.cantidad = cantidad;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+
+}
