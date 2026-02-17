bc30fb6b1bec4d998bb4695f1a5da40ff2ae4ecd
JuanBoullosa <151090164+JuanBoullosa@users.noreply.github.com>
2026-02-17 14:04:00 -0300

    Commit

--- Pasteleria/src/logica/colecciones/Postres.java
+++ Pasteleria/src/logica/colecciones/Postres.java
@@ -4,32 +4,32 @@ import logica.negocio.Postre;
 
 public class Postres {
 	
-	private TreeMap<String,Postre> abb;
+	private TreeMap<String,Postre> arbolpostres;
 	
 
 	public Postres ()
 	{
-		abb = new TreeMap<String, Postre> ();
+		arbolpostres= new TreeMap<String, Postre> ();
 	}
 
 	public boolean member(String codigo)
 	{
-		return abb.containsKey(codigo);
+		return arbolpostres.containsKey(codigo);
 	}
 	
 	public Postre find (String codigo)
 	{
-		return abb.get(codigo);
+		return arbolpostres.get(codigo);
 	}
 	
 	public void insert (String codigo, Postre P)
 	{
-		abb.put(codigo, P);
+		arbolpostres.put(codigo, P);
 	}
 	
 	public void detelet (String codigo)
 	{
-		abb.remove(codigo);
+		arbolpostres.remove(codigo);
 	}
 	
 }
--- Pasteleria/src/logica/colecciones/Ventas.java
+++ Pasteleria/src/logica/colecciones/Ventas.java
@@ -1,5 +1,32 @@
 package logica.colecciones;
+import java.util.LinkedList;
+import logica.negocio.Venta;
 
 public class Ventas {
 
+	private LinkedList <Venta> listaVentas;
+
+	//Constructor
+	public Ventas()
+	{
+		listaVentas = new LinkedList <Venta> ();
+		
+	}
+
+	//agregar por el final
+	public void insBack(Venta V){
+	listaVentas.addLast(V);
+	}
+
+	//Getter y setters
+	public LinkedList<Venta> getListaVentas() {
+		return listaVentas;
+	}
+
+	public void setListaVentas(LinkedList<Venta> listaVentas) {
+		this.listaVentas = listaVentas;
+	}
+	
+	
+	
 }
