0dc5ff031bd8f6f68eaf68b747f9fcf83ded3218
JuanBoullosa <151090164+JuanBoullosa@users.noreply.github.com>
2026-02-22 10:13:49 -0300

    Prueba 2

--- Pasteleria/src/logica/colecciones/Ventas.java
+++ Pasteleria/src/logica/colecciones/Ventas.java
@@ -1,13 +1,14 @@
 package logica.colecciones;
 import java.util.ArrayList;
 import java.util.LinkedList;
-
 import logica.negocio.Light;
 import logica.negocio.Postre;
 import logica.negocio.Venta;
 import logica.valueobjects.VOLight;
 import logica.valueobjects.VOPostre;
+import logica.valueobjects.VOPostreDetallado;
 import logica.valueobjects.VOVenta;
+
 
 public class Ventas {
 
@@ -21,10 +22,19 @@ public class Ventas {
 	}
 
 	//agregar por el final
-	public void insBack(Venta V){
-	listaVentas.addLast(V);
+	public void insBack(Venta V)
+	{
+		listaVentas.addLast(V);
 	}
+	
+	//agregar por el final
+	public void insBackVOVentaDetalle(Venta V)
+	{
+		listaVentas.addLast(V);
+	}
+	
 
+	//Muestra lo que cargas a mano
 	public ArrayList<VOVenta> obtenerVentas() {
 		ArrayList<VOVenta> listaVentasObtenido = new ArrayList<VOVenta>();
 		
@@ -33,6 +43,19 @@ public class Ventas {
 			listaVentasObtenido.add(vov);		
 		}
 		return listaVentasObtenido;
+		}
+	/*
+	//Requerimiento 4
+	public ArrayList<VOVenta> obtenerVentaIngreso (){
+		ArrayList<VOVenta> ListaDeVentaIngreso = new ArrayList<VOVenta> ();
+		for (Venta v: listaVentas) {
+			
+		}
+		
+	
 	}
+	*/
+	
+	
 	
 }
