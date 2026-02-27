705d779434b71f3fc9934d830c3bfd6fe6c4598a
Fede1891 <160802959+Fede1891@users.noreply.github.com>
2026-02-27 02:33:25 -0300

    act

--- Pasteleria/src/config/Configuracion.java
+++ Pasteleria/src/config/Configuracion.java
@@ -1,5 +1,44 @@
 package config;
 
-public class Configuracion {
+import java.util.Properties;
+import java.io.*;
 
+public class Configuracion {
+	
+	private String ipServidor;
+    private String puertoServidor;
+    private String rutaRespaldo;
+
+    public Configuracion() {
+        try {
+            Properties p = new Properties();
+            String nomArch = "config/sistema.properties";
+
+            // Abro el archivo properties y leo los datos de configuración
+            p.load(new FileInputStream(nomArch));
+
+            ipServidor = p.getProperty("ipServidor");
+            puertoServidor = p.getProperty("puertoServidor");
+            rutaRespaldo = p.getProperty("rutaRespaldo");
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String getIpServidor() 
+    {
+        return ipServidor;
+    }
+
+    public String getPuertoServidor() 
+    {
+        return puertoServidor;
+    }
+    
+    public String getrutaRespaldo() 
+    {
+        return rutaRespaldo;
+    }
 }
